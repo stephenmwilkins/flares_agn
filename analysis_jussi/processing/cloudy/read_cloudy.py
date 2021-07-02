68144846f43f13c038a73272409438da864f1337
Jussi K. Kuusisto <jkk32@sussex.ac.uk>
2021-07-02 15:20:49 +0100

    Updating the repo with cloudy output analysis/plotting scripts

--- analysis_jussi/processing/cloudy/read_cloudy.py
+++ analysis_jussi/processing/cloudy/read_cloudy.py
@@ -1,9 +1,12 @@
 import numpy as np
 
+output_dir = 'output/linear'
 
-i = 1 # model index (should start at 1)
+AGN_T = np.linspace(10000, 1.5e6, 100) # range of AGN temperatures to model
 
-lam, incident, transmitted, nebular, total, linecont  = np.loadtxt(f'{output_dir}/{i}.cont'), delimiter='\t', usecols = (0,1,2,3,4,8)).T
+for i in range(len(AGN_T)):
+    lam, incident, transmitted, nebular, total, linecont  = np.loadtxt(f'{output_dir}/{i}.cont', delimiter='\t', usecols = (0,1,2,3,4,8)).T
+
 
 # lam = wavelength
 # incident = should be the input spectrum
